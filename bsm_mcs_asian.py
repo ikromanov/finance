c50939f1d5a8af595b1691e02e1007ddc258a6ef
ikromanov <ikromanov@gmail.com>
2015-11-08 10:00:04 -0500

    writing to file

--- bsm_mcs_asian.py
+++ bsm_mcs_asian.py
@@ -7,6 +7,7 @@ import numpy as np
 import scipy.stats
 import math
 from time import time
+from tabulate import tabulate
 
 # Parameter Values
 S0 = 110.  # initial index level
@@ -17,7 +18,7 @@ sigma = 0.2  # volatility
 
 M = 12 ; # number of time steps
 dt = T / M;
-I = 1000000  # number of simulations
+I = 100000  # number of simulations
 
 # Valuation function
 def AsianCallSimPrice(S0, K, T, r, sigma, M, I, CV = False):
@@ -31,6 +32,8 @@ def AsianCallSimPrice(S0, K, T, r, sigma, M, I, CV = False):
     Savg = np.average(S, axis = 0)
     if CV == False:
         price = np.exp(-r * T) * np.sum(np.maximum(Savg - K, 0)) / I
+        error = math.sqrt(np.var(np.maximum(Savg - K, 0))) / math.sqrt(I)
+        result = (price, error)
     else: 
         Tvector = np.arange(dt, T + dt, dt)
         T_avg = Tvector.mean()
@@ -48,7 +51,10 @@ def AsianCallSimPrice(S0, K, T, r, sigma, M, I, CV = False):
         Y = np.exp(-r * T) * np.maximum(Savg - K, 0)
         b = np.cov(X,Y)[0][1] / X.var()
         price = Y.mean() - b * (X.mean() - GeomAsianCall)
-    return price
+        error = math.sqrt(np.var(Y - b*X)) / math.sqrt(I)
+        rho = np.corrcoef(X,Y)[0][1]
+        result = (price, error, rho)
+    return result
 
 
 # Calculating execution time
@@ -56,9 +62,23 @@ t0 = time()
 CallPrice = AsianCallSimPrice(S0, K, T, r, sigma, M, I)
 CallPriceCV = AsianCallSimPrice(S0, K, T, r, sigma, M, I,True)
 tnp1 = time() - t0
-# Result Output
-print "Value of the Asian Call Option %5.3f" % CallPrice
-print "Value of the Asian Call Option (with CV) %5.3f" % CallPriceCV
-print "Duration in seconds %5.3f" % tnp1
 
 # Results table
+N_sim = [1000, 10000, 100000, 1000000]
+res = []
+for i in range(0, len(N_sim)):
+    newrow = [N_sim[i], 
+            AsianCallSimPrice(S0, K, T, r, sigma, M, N_sim[i])[0],
+            AsianCallSimPrice(S0, K, T, r, sigma, M, N_sim[i])[1],
+            AsianCallSimPrice(S0, K, T, r, sigma, M, N_sim[i], True)[0], 
+            AsianCallSimPrice(S0, K, T, r, sigma, M, N_sim[i], True)[1],
+            AsianCallSimPrice(S0, K, T, r, sigma, M, N_sim[i], True)[2]]
+    res.append(newrow)
+
+table = tabulate(res, headers=["N", "Price", "Error", 
+                             "PriceCV", "ErrorCV", "Corr"])
+print(table)
+# Write to file
+f = open('bsm_mcs_asian_output.txt', 'w')
+f.write(table)
+f.close()
